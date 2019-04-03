721350f0b3c5e6e6ac7516a4129aae9b76eb0368
sarL3y <matt.tapper@gmail.com>
2019-04-02 21:59:13 -0700

    Add "public" test // Init TravisCI commit

--- test/test-server.js
+++ test/test-server.js
@@ -10,3 +10,22 @@ const expect = chai.expect;
 
 chai.use(chaiHttp);
 
+describe('Public', function() {
+
+    before(function() {
+        return runServer(TEST_DATABASE_URL);
+    });
+
+    after(function() {
+        return closeServer;
+    });
+    
+    it('should serve "public" files', function() {
+        return chai.request(app)
+            .get('/')
+            .then(function(res) {
+                expect(res).to.have.status(200);
+            });
+    });
+});
+
