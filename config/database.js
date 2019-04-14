df1349e0b50407ebc991e0b5fe5c0a989cc82b28
sarL3y <matt.tapper@gmail.com>
2019-04-14 13:44:11 -0700

    Style updates

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 'use strict';
 
-exports.DATABASE_URL = /*process.env.DATABASE_URL ||*/ 'mongodb://localhost/';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://moveoutadmin:moveoutadmin@moveoutdb-lzomt.mongodb.net/moveoutdb?retryWrites=true';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb+srv://moveoutadmin:moveoutadmin@moveoutdb-lzomt.mongodb.net/moveoutdbtest?retryWrites=true';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
